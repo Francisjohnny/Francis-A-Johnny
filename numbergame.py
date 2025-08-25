448f92dfb011ca73b74a8028fe89df55a77ea847
Francis A Johnny <john2frances@gmail.com>
2025-08-25 01:48:59 +0100

    Local v Global Scope 25062025

--- numbergame.py
+++ numbergame.py
@@ -3,12 +3,16 @@ from random import randint
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
+turns = 0
 # Function to check users' guess against actual answer
 def check_answer(user_guess, actual_answer):
     if user_guess > actual_answer:
         print("Too high.")
+        global turns
+        turns -= 1
     elif user_guess < actual_answer:
         print("Too low.")
+        turns -= 1
     else:
         print(f"You got it right! The answer was {actual_answer}")
 
@@ -22,27 +26,25 @@ def set_difficulty():
         return HARD_LEVEL_TURNS
 
 
+def game():
+    # Choosing a random number between 1 and 100.
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    print(f"Pssst, the correct answer is {answer}")
 
 
-# Choosing a random number between 1 and 100.
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-answer = randint(1, 100)
-print(f"Pssst, the correct answer is {answer}")
+    turns = set_difficulty()
+    print(f"You have {turns} attempts remaining to guess the number.")
 
+    # Repeat the guessing functionality if they are wronge
+    guess = 0
+    while guess != answer:
+        # Let the user guess a number
+        guess = int(input("Make a guess: "))
 
-turns = set_difficulty()
-print(f"You have {turns} attempts remaining to guess the number.")
+    check_answer(guess, answer)
 
-while guess != answer:
-    # Let the user guess a number
-    guess = int(input("Make a guess: "))
+    # Tracking the number of turns and reduce by 1 if they get it wrong
 
-check_answer(guess, answer)
-
-
-
-
-# Tracking the number of turns and reduce by 1 if they get it wrong
-
-# Repeat the guessing functionality if they are wronge
+game()
